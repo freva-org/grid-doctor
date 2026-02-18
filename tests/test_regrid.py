314819802ca425b9cf6f972ac7076d390acf7bec
Manuel Reis <reis@dkrz.de>
2026-02-18 18:48:14 +0100

    tests: add era5 mock and test latlon_to_healpix_pyramid

--- tests/test_regrid.py
+++ tests/test_regrid.py
@@ -7,30 +7,32 @@ from grid_doctor.helpers import regrid_to_healpix, latlon_to_healpix_pyramid
 
 @pytest.mark.parametrize(
     "test_ds",
-    ["regular", "curvilinear"],
-    ids=["regular", "curvilinear"],
+    ["regular", "curvilinear", "era5"],
+    ids=["regular", "curvilinear", "era5"],
     indirect=True,
 )
 def test_regrid_to_healpix(test_ds):
     hp_ds = regrid_to_healpix(test_ds, level=9)
 
+    print(test_ds)
+    print(hp_ds)
     assert all((hp_ds.attrs[k] == v for k, v in test_ds.attrs.items()))
 
-    assert len(test_ds.data_vars) == len(hp_ds.data_vars)
+    assert set(test_ds.data_vars).issuperset(hp_ds.data_vars.keys())
 
     attrs_eq = lambda a, b: a.attrs == b.attrs
     var_check = lambda v, l: l(test_ds[v], hp_ds[v])
 
     dtype_eq = lambda a, b: a.dtype == b.dtype
-    for v in test_ds.data_vars:
+    for v in hp_ds.data_vars:
         assert var_check(v, attrs_eq)
         assert var_check(v, dtype_eq)
 
 
 @pytest.mark.parametrize(
     "test_ds",
-    ["regular", "curvilinear"],
-    ids=["regular", "curvilinear"],
+    ["regular", "curvilinear", "era5"],
+    ids=["regular", "curvilinear", "era5"],
     indirect=True,
 )
 def test_latlon_to_healpix_pyramid(test_ds):
