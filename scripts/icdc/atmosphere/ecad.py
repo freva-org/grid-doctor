e3129d1f1a94c791e07e609e68f62d55f84dbb8d
Manuel Reis <reis@dkrz.de>
2026-03-09 14:01:26 +0100

    icdc: refactor ECAD

--- scripts/icdc/atmosphere/ecad.py
+++ scripts/icdc/atmosphere/ecad.py
@@ -1,61 +1,37 @@
-import logging
+from icdc.base import Config, Pipeline
 
-from argparse import ArgumentParser
-from icdc.base import BaseStructure
-from os import getenv
+from grid_doctor import latlon_to_healpix_pyramid
 
-class ECAD(BaseStructure):
-    _open_kwargs = {'parallel':True, 'engine':'h5netcdf', 'join':'left'} 
-    def __init__(self):
-        super().__init__({
-            'ECAD/mean/' :  f'/pool/data/ICDC/atmosphere/ecad_eobs/DATA/t*ens_mean_0.1deg_reg_v31.0e.nc',
-            'ECAD/spread/' :  f'/pool/data/ICDC/atmosphere/ecad_eobs/DATA/t*ens_spread_0.1deg_reg_v31.0e.nc',
+ECADSpecMean = Config(
+    dst_s3url="s3://icdc/healpix/atmosphere/ECAD/P1D/mean/",
+    paths="/pool/data/ICDC/atmosphere/ecad_eobs/DATA/t*ens_mean_0.1deg_reg_v31.0e.nc",
+    engine="netcdf-4",
+    parallel=False,
+    chunking={"time": 48},
+    regrid_function=lambda x: latlon_to_healpix_pyramid(x, keep_nans=True),
+    init=True,
+    region={"time": slice(0, 96)},
+    zarr_format=2,
+)
 
-        })
-    
-    @classmethod
-    def chunking(cls):
-        return {'time':1}
-
-    def convert(self, init: bool = True, region:dict = {'time':slice(0,1)}):
-        from grid_doctor import (
-            latlon_to_healpix_pyramid,
-        )
-        dst_url = 's3://icdc/healpix/atmosphere/'
-        pyramids = {} 
-        for name, ds in self.items():
-            print(ds)
-            ds_hp = latlon_to_healpix_pyramid(ds.chunk(self.chunking()), method='nearest', keep_nans=True)
-            pyramids[ dst_url + name ] = ds_hp
-        
-        self.write(pyramids, init=init, region=region)
+ECADSpecSpread = Config(
+    dst_s3url="s3://icdc/healpix/atmosphere/ECAD/P1D/spread/",
+    paths="/pool/data/ICDC/atmosphere/ecad_eobs/DATA/t*ens_spread_0.1deg_reg_v31.0e.nc",
+    engine="netcdf-4",
+    parallel=False,
+    chunking={"time": 48},
+    regrid_function=latlon_to_healpix_pyramid,
+    init=True,
+    region={"time": slice(0, 96)},
+    zarr_format=2,
+)
 
 
-def main():
-    start_idx = int(getenv("SLURM_ARRAY_TASK_ID", 0))
-    parser = ArgumentParser()
-    parser.add_argument("--init", action="store_true")
-    parser.add_argument("--slice-size", default=48, type=int)
-    parser.add_argument("--start", default=start_idx, type=int)
-    parser.add_argument("--debug", action="store_true")
+def run():
+    for spec in (ECADSpecMean, ECADSpecSpread):
+        ECADPipeline = Pipeline(spec)
+        ECADPipeline.run()
 
-    args = parser.parse_args()
 
-    logging.getLogger().setLevel(logging.DEBUG if args.debug else logging.INFO)
-
-    if args.slice_size % ECAD.chunking()['time']:
-        print("slice-size must be a multiple of 48 (time chunk)")
-        exit(1)
-
-    region = {
-        "time": slice(args.start * args.slice_size, (args.start + 1) * args.slice_size)
-    }
-    print(f"init={args.init}, region={region}")
-    ecad = ECAD()
-    ecad.convert(init=args.init, region=region)
-
-    
-
-        
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    run()
