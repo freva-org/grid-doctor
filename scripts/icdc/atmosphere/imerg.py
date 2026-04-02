1354fac3e6b571d57184a77e6055a4712519073a
Manuel Reis <reis@dkrz.de>
2026-04-02 09:29:31 +0200

    icdc: patch IMERG config

--- scripts/icdc/atmosphere/imerg.py
+++ scripts/icdc/atmosphere/imerg.py
@@ -5,8 +5,8 @@ from grid_doctor import latlon_to_healpix_pyramid
 class IMERG(Collection):
     PT30M = Config(
         dst_s3url="s3://icdc/healpix/atmosphere/IMERG/PT30M/",
-        paths="/pool/data/ICDC/atmosphere/imerg/DATA/2025/IMERG_precipitationrate__V07B__halfhourly__0.1degree__*.nc",
-        engine="netcdf-4",
+        paths="/pool/data/ICDC/atmosphere/imerg/DATA/*/IMERG_precipitationrate__V07B__halfhourly__0.1degree__*.nc",
+        engine="netcdf4",
         parallel=False,
         chunking={"time": 48},
         regrid_function=latlon_to_healpix_pyramid,
